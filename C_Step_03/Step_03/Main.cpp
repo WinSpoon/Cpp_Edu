2c065bf6956b12caf975d1457e77ccaa7e0828a9
WinSpoon <winstone851108@gmail.com>
2022-04-01 19:35:12 +0900

    Step_03

---
+++ C_Step_03/Step_03/Main.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	/*
+	// ** 변수 선언
+	int a;
+	int b;
+
+	// ** 선언과 동시에 초기화
+	int c = 30, d = 40;
+
+	// ** 초기화
+	a = 10;
+	b = 20;
+
+	printf_s("%d %d \n", a, b);
+	printf_s("%d %d \n", c, d);
+	*/
+
+
+
+
+
+
+
+
+	/*
+	// ** 변수 선언
+	float fNumber;
+
+	// ** 변수 초기화
+	fNumber = 87.654321;
+
+	// ** 출력
+	printf_s("%f\n", fNumber);	// 형태에 알맞은 출력
+	printf_s("%d\n", (int)fNumber);	// float형을 int형으로 변환
+	printf_s("%c\n", (char)fNumber);	// float형을 char형으로 변환
+	printf_s("%d\n", fNumber);	// 형태에 알맞지 않은 출력. 쓰레기 값 출력.
+	*/
+
+
+
+
+
+
+	/*
+	// ** 실수형 타입의 변수를
+	float fPI = 3.141592f;
+
+
+	// ** 정수형 타입의 변수에 대입하면
+	// ** 소수점 데이터는 소실 된다.
+	int i = fPI;
+
+	printf_s("%02d\n", i);  // 3
+	printf_s("%0.3f\n", fPI);
+	*/
+
+
+
+	return 0;
+}
