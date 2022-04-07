d5f7cdeff9e072058d8c38a03355970f3be64794
WinSpoon <winstone851108@gmail.com>
2022-04-07 19:56:24 +0900

    Step_07

---
+++ C_Step_07/C_Step_07/Main.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    /*
+    int iArrayA[5] = { 1, 2, 3, 4, 5 };
+    int iArrayB[] = { 1, 2, 3, 4, 5 };
+    int iArrayC[5] = { 1, 2 };
+    int iArrayD[5] = { 0 };
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf_s("%d¹ø-[A : %d] [B : %d] [C : %d] [D : %d]\n\n", 
+            i, iArrayA[i], iArrayB[i], iArrayC[i], iArrayD[i]);
+    }
+    */
+    
+
+
+
+    /*
+    int iArray[3][3] =
+    {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf_s("%d\t", iArray[i][j]);
+
+            if (j == 2)
+            {
+                printf_s("\n");
+            }
+        }
+    }
+    
+    printf_s("\n");
+    */
+
+
+
+
+	return 0;
+}
